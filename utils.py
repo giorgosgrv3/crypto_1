73c4a9bc797704ff5678c347941f6cdb95eb5cdc
giorgosgrv3 <giorgosgrv3@gmail.com>
2026-03-23 01:12:53 +0100

    added 4a, 4b, 4c

--- utils.py
+++ utils.py
@@ -57,3 +57,49 @@ def solve_linear_congruence(a, b, n):
     x0 = (x_inv * (b // gcd)) % n_prime
 
     return x0
+
+def efficient_pow(base, exponent, modulus):
+    """ calculate base^exponent (mod modulus) efficiently,
+    using the algorithm from our notes (2.11, 2.12).
+    """
+    if modulus == 1:
+        return 0
+
+    #property 7a from 2.3 : r_n[a] congruent to a, mod n.
+    result = 1 #initializing with neutral element
+    base = base % modulus #ensure base is small
+
+    while exponent > 0: #iterate all bits, from LSB to MSB
+        if exponent % 2 == 1: #if odd, current bit=1, multiply into result. otherwise do not.
+            result = (result * base) % modulus
+        exponent >>= 1 #shift 1bit to the right
+        base = (base * base) % modulus #square the base, happens every iteration no matter what
+    return result
+
+def mod_inverse(a, m):
+    '''return the mod inverse of a (mod m)'''
+    
+    gcd, x, y = extended_euclidean(a, m)
+    if gcd != 1:
+        raise ValueError(f"Modular inverse does not exist for {a} mod {m}")
+    
+    return x % m #ensure smallest remainder in Zm
+
+def solve_crt(list_xi, list_ni, total_n):
+    '''we apply the CRT to find a unique solution for a system of
+    linear congruences'''
+    x = 0
+    # Build the solution using the CRT summation formula
+    for i in range(len(list_ni)):
+        ni = list_ni[i]
+        xi = list_xi[i]
+        
+        # Mi = n/ni
+        Mi = total_n // ni
+        yi = mod_inverse(Mi, ni)
+        #yi = solve_linear_congruence(Mi, 1, ni) would also work here but okay
+        
+        #computation of the secret, we apply mod n to keep it efficient
+        x = (x + xi * Mi * yi) % total_n
+        
+    return x
