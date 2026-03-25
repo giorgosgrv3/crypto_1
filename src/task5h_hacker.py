b39968b446fe3f1d59dc316ab9f83128b8a7daf6
giorgosgrv3 <giorgosgrv3@gmail.com>
2026-03-25 04:20:04 +0200

    small changes to 5h

--- src/task5h_hacker.py
+++ src/task5h_hacker.py
@@ -41,6 +41,8 @@ if resp != "Fail":
         if status == "Fail":
             print(f"round {i+1} failed (this shouldn't have happened)")
             break
+        elif status == "Again":
+            print(f"round {i+1} successful")
         elif status == "Success":
             print("-- we have logged in as the user.\n")
             print("Hello, friend. Hello, friend? That's lame. Maybe I should give you a name. But that's a slippery slope. You're only in my head. ")
